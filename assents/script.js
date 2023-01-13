dd075610c97b4c24b327526d17bf866ee35dc7cf
Rian-usr <rian.jesus5@etec.sp.gov.br>
2023-01-13 16:39:15 -0300

    atualizando

--- assents/script.js
+++ assents/script.js
@@ -1,5 +1,9 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
+const restart = document.querySelector('.restart')
+
+var myScore;
 
 const jump = () => {
     mario.classList.add('jump');
@@ -12,18 +16,24 @@ const jump = () => {
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPosition = clouds.offsetLeft;
 
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 85) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
+        
         mario.style.animation ='none';
         mario.style.bottom = `${marioPosition}px`;
 
         mario.src= './img/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
+
+        restart.style.display = 'block';
 
         clearInterval(loop);
     }
